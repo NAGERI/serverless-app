4e4599952a44b94d2b9b2db764923fb7d3a08e3a
SPIF KLA - Cedric Nageri <Cedric.Nageri@cevalogistics.com>
2022-10-15 23:06:01 +0300

    feat: validate Incoming HTTP requests are validated in Lambda handlers

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'mwx51k3kdl'
+const apiId = 'mmo1udxz2m'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
